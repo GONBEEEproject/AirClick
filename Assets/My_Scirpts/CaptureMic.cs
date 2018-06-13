6681c5b16ec7f91f189b2144a72c4be5553c67f3
GONBEEE_project_myhouse0 <gonbeee.project@gmail.com>
2018-06-14 00:32:54 +0900

    いろいろついか

---
+++ Assets/My_Scirpts/CaptureMic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureMic : MonoBehaviour {
+
+    private AudioSource AS;
+
+    [SerializeField]
+    private GameObject cube;
+
+    private bool isCube=false;
+
+	// Use this for initialization
+	void Start () {
+        AS = GetComponent<AudioSource>();
+        AS.clip = Microphone.Start(null, true, 999, 44100);
+        AS.loop = true;
+
+        while (!(Microphone.GetPosition("") > 0)) { }
+        AS.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float vol = GetAverageVolume();
+        Debug.Log(vol);
+
+        if (vol > 0.05)
+        {
+            isCube=!isCube;
+            cube.SetActive(isCube);
+        }
+
+	}
+
+    float GetAverageVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        AS.GetOutputData(data, 0);
+        foreach(float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+
+        return a / 256.0f;
+    }
+
+
+}
---
+++ Assets/My_Scirpts/Hand.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+
+public class Hand : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject handObj;
+    private MeshRenderer handRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+        InteractionManager.InteractionSourceUpdated += InteractionSourceUpdated;
+        InteractionManager.InteractionSourcePressed += InteractionSourcePressed;
+        InteractionManager.InteractionSourceReleased += InteractionSourceReleased;
+        InteractionManager.InteractionSourceLost += InteractionSourceLost;
+
+        handRenderer = handObj.GetComponent<MeshRenderer>();
+	}
+	
+    //手だして
+    private void InteractionSourceUpdated(InteractionSourceUpdatedEventArgs eventArgs)
+    {
+        handRenderer.enabled = true;
+        
+        Vector3 position;
+        if(eventArgs.state.sourcePose.TryGetPosition(out position))
+        {
+            handObj.transform.position = position;
+        }
+    }
+
+    //指さげて
+    private void InteractionSourcePressed(InteractionSourcePressedEventArgs eventArgs)
+    {
+
+    }    
+
+    //指あげて
+    private void InteractionSourceReleased(InteractionSourceReleasedEventArgs eventArgs)
+    {
+
+    }
+
+    //手ださないで
+    private void InteractionSourceLost(InteractionSourceLostEventArgs eventArgs)
+    {
+        handRenderer.enabled = false;
+    }
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
